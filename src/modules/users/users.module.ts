6d18ad3bd5dfeedeb58db7459446a48b2bd72ef6
Adewole oluwanifemi <beniphemeh11@yahoo.com>
2025-02-03 10:00:59 +0100

    sequelizeModule install

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -4,6 +4,7 @@ import { modelInstances } from 'src/core/model-instances';
 import { UsersController } from './users.controller';
 
 @Module({
+  // imports: [SequelizeModule.forFeature([...modelInstances])],
   controllers: [UsersController],
   providers: [UsersService, ...modelInstances],
   exports: [UsersService],
