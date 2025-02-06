e2ba611ea9eba8de3cfddd038a4d3c2f9fda8626
Adewole oluwanifemi <beniphemeh11@yahoo.com>
2025-02-06 10:42:18 +0100

    update endpoint

--- src/modules/users/admin-users.controller.ts
+++ src/modules/users/admin-users.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, Delete, Get, Param, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { AuthGuard } from '@nestjs/passport';
+import { AdminGuard } from 'src/core/guards/admin.guard';
 
-@UseGuards(AuthGuard('jwt'))
+@UseGuards(AuthGuard('jwt'), AdminGuard)
 @Controller('admin/users')
 export class AdminUsersController {
   constructor(private readonly usersService: UsersService) {}
