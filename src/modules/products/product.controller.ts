e908c92a3f5c648dd718043ca60bee91f1cf7642
Adewole oluwanifemi <beniphemeh11@yahoo.com>
2025-02-12 14:03:27 +0100

    creating product bug

--- src/modules/products/product.controller.ts
+++ src/modules/products/product.controller.ts
@@ -1,18 +1,19 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { ProductService } from './product.service';
 import { AuthUser } from 'src/core/interfaces';
+import { JwtGuard } from '../auth/jwt-guard';
 // import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
-
+  @UseGuards(JwtGuard)
   @Post()
   async create(
     @Body() createProductDto: CreateProductDto,
     @Req() req: AuthUser,
   ) {
-    return this.productService.create(createProductDto, req);
+    return this.productService.create(createProductDto);
   }
 }
