6ddf329cb52d08c7a62fdbc32192f2b7949cfda3
Adewole oluwanifemi <beniphemeh11@yahoo.com>
2025-03-01 08:32:17 +0100

    middleware login JWTKEY fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { MailModule } from './core/mail/mail.module';
 import { ProductsModule } from './modules/products/product.module';
-import { JwtService } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { LoggerMiddleware } from './core/middleware/loggermiddleware';
 import { ApiLoggerModule } from './core/middleware/api-builder-logger/api-logger.module';
 
