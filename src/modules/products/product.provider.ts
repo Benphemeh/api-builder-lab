069733bc524c83826c7ff1fd07f815e3e96c8f11
Adewole oluwanifemi <beniphemeh11@yahoo.com>
2025-03-06 16:45:31 +0100

    product update email added

--- src/modules/products/product.provider.ts
+++ src/modules/products/product.provider.ts
@@ -1,11 +1,17 @@
 import { REPOSITORY } from 'src/core/constants';
-import { Product } from 'src/core/database';
+import { Product, User } from 'src/core/database';
 import { ProductService } from './product.service';
+import { UsersService } from '../users/users.service';
 
 export const productProviders = [
   {
     provide: REPOSITORY.PRODUCT,
     useValue: Product,
   },
+  {
+    provide: REPOSITORY.USER,
+    useValue: User,
+  },
   ProductService,
+  UsersService,
 ];
