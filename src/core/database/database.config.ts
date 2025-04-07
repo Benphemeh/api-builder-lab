d7f9080fcd0ba5c84c9af72d1e1146727819d0c8
Adewole oluwanifemi <beniphemeh11@yahoo.com>
2025-04-07 08:56:33 +0100

    installed dotenv-cli to trying to fix the database config error

--- src/core/database/database.config.ts
+++ src/core/database/database.config.ts
@@ -1,30 +1,67 @@
-import * as dotenv from 'dotenv';
-import { IDatabaseConfig } from './interfaces/dbConfig.interface';
+import { config } from 'dotenv';
+import { Dialect } from 'sequelize';
 
-dotenv.config();
+config();
 
-export const databaseConfig: IDatabaseConfig = {
+const dbConfig = {
   development: {
+    url: process.env.DB_URL,
     username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME_DEVELOPMENT,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
+    port: Number(process.env.DB_PORT),
+    dialect: process.env.DB_DIALECT! as Dialect,
   },
   test: {
+    url: process.env.DB_URL,
     username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME_TEST,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
+    port: Number(process.env.DB_PORT),
+    dialect: process.env.DB_DIALECT! as Dialect,
   },
   production: {
+    url: process.env.DB_URL,
     username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME_PRODUCTION,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    port: Number(process.env.DB_PORT),
+    dialect: process.env.DB_DIALECT! as Dialect,
   },
 };
+
+export default dbConfig;
+
+// import * as dotenv from 'dotenv';
+// import { IDatabaseConfig } from './interfaces/dbConfig.interface';
+
+// dotenv.config();
+
+// export const databaseConfig: IDatabaseConfig = {
+//   development: {
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME_DEVELOPMENT,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: process.env.DB_DIALECT,
+//   },
+//   test: {
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME_TEST,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: process.env.DB_DIALECT,
+//   },
+//   production: {
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME_PRODUCTION,
+//     host: process.env.DB_HOST,
+//     dialect: process.env.DB_DIALECT,
+//   },
+// };
