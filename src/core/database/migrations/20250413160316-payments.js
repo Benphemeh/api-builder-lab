4fb59bd4584f8b24642b4927713637c7c7674fcf
Adewole oluwanifemi <beniphemeh11@yahoo.com>
2025-04-21 05:23:22 +0100

    order and payment base-migration fixed

--- src/core/database/migrations/20250413160316-payments.js
+++ src/core/database/migrations/20250413160316-payments.js
@@ -30,17 +30,17 @@ module.exports = {
         type: Sequelize.FLOAT,
         allowNull: false,
       },
-      createdAt: {
+      created_at: {
         type: Sequelize.DATE,
         allowNull: false,
         defaultValue: Sequelize.NOW,
       },
-      updatedAt: {
+      updated_at: {
         type: Sequelize.DATE,
         allowNull: false,
         defaultValue: Sequelize.NOW,
       },
-      deletedAt: {
+      deleted_at: {
         type: Sequelize.DATE,
         allowNull: true,
       },
