40d92ff223cf1f5e5beba45f3fe83eb91f1328f2
Adewole oluwanifemi <beniphemeh11@yahoo.com>
2025-04-28 17:00:14 +0100

    payment dtos validator messages

--- src/modules/payment/dto/update-payment.dto.ts
+++ src/modules/payment/dto/update-payment.dto.ts
@@ -2,6 +2,6 @@ import { IsOptional, IsString } from 'class-validator';
 
 export class UpdatePaymentDto {
   @IsOptional()
-  @IsString({ message: 'Status must be a string' })
+  @IsString()
   status?: 'pending' | 'success' | 'failed';
 }
