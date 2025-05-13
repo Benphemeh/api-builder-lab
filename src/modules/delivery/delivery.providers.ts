1628147d02b1dfc7ba02e391a97ad368c3c72914
Adewole oluwanifemi <beniphemeh11@yahoo.com>
2025-05-13 12:35:58 +0100

    removec Order from delivery providers

--- src/modules/delivery/delivery.providers.ts
+++ src/modules/delivery/delivery.providers.ts
@@ -1,5 +1,4 @@
 import { REPOSITORY } from 'src/core/constants';
-import { Order } from 'src/core/database';
 import Delivery from 'src/core/database/models/delivery.model';
 import { MailService } from 'src/core/mail/mail.service';
 
@@ -7,10 +6,6 @@ export const deliveryProviders = [
   {
     provide: REPOSITORY.DELIVERY,
     useValue: Delivery,
-  },
-  {
-    provide: REPOSITORY.ORDER,
-    useValue: Order,
   },
   MailService,
 ];
--- src/modules/delivery/delivery.service.ts
+++ src/modules/delivery/delivery.service.ts
@@ -5,27 +5,15 @@ import { CreateDeliveryDto } from './dto/create-delivery.dto';
 import { REPOSITORY } from 'src/core/constants';
 import { UpdateDeliveryStatusDto } from './dto/update-delivery.dto';
 import { MailService } from 'src/core/mail/mail.service';
-import { Order } from 'src/core/database';
 
 @Injectable()
 export class DeliveryService {
   constructor(
     @Inject(REPOSITORY.DELIVERY)
     private readonly deliveryRepository: typeof Delivery,
-    @Inject(REPOSITORY.ORDER)
-    private readonly orderRepository: typeof Order,
     private readonly mailService: MailService,
   ) {}
 
-  // async createDelivery(dto: CreateDeliveryDto): Promise<Delivery> {
-  //   console.log(`Creating delivery for order ID: ${dto.orderId}`);
-  //   const order = await this.orderRepository.findByPk(dto.orderId);
-  //   if (!order) {
-  //     throw new NotFoundException(`Order with ID ${dto.orderId} not found`);
-  //   }
-
-  //   return this.deliveryRepository.create({ ...dto });
-  // }
   async createDelivery(dto: CreateDeliveryDto): Promise<Delivery> {
     const delivery = await this.deliveryRepository.create({ ...dto });
     return delivery;
@@ -45,7 +33,6 @@ export class DeliveryService {
 
     const updatedDelivery = await delivery.update({ status: dto.status });
 
-    // Fetch related order and user details
     const order = delivery.order;
     const user = await order?.user;
 
