cfef68a57a626e6c0e588b1d60541ca2b093733e
Adewole oluwanifemi <beniphemeh11@yahoo.com>
2025-05-19 15:16:37 +0100

    order status enum

--- src/modules/order/dto/update-order.dto.ts
+++ src/modules/order/dto/update-order.dto.ts
@@ -1,9 +1,10 @@
 import { IsOptional, IsEnum } from 'class-validator';
+import { ORDER_STATUS } from 'src/core/enums';
 
 export class UpdateOrderDto {
   @IsOptional()
-  @IsEnum(['pending', 'completed', 'canceled'], {
+  @IsEnum(ORDER_STATUS, {
     message: 'Status must be one of: pending, completed, or canceled',
   })
-  status?: 'pending' | 'completed' | 'canceled';
+  status?: ORDER_STATUS;
 }
