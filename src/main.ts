77b0747af0191590d4b257d10640aa88769f02d4
Adewole oluwanifemi <beniphemeh11@yahoo.com>
2025-05-31 07:15:06 +0100

    swaggwer main.ts configuration

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
     .setDescription('API documentation for the O’Ben Brands project')
     .setVersion('1.0')
     .addTag('O’Ben')
-    .addBearerAuth() // If you're using JWT Authentication
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
@@ -34,20 +34,3 @@ async function bootstrap() {
   await app.listen(3000);
 }
 bootstrap();
-
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import { ValidateInputPipe } from './core/pipes/validate.pipe';
-// import * as dotenv from 'dotenv';
-// import * as bodyParser from 'body-parser';
-
-// dotenv.config();
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   app.setGlobalPrefix('api');
-//   app.useGlobalPipes(new ValidateInputPipe());
-//   app.use('/api/payments/webhook', bodyParser.raw({ type: '*/*' }));
-//   await app.listen(3000);
-// }
-// bootstrap();
