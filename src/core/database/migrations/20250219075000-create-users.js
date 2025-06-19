0b3016b264209cf1ef50bea7e97ff1b92e20dc0f
Adewole oluwanifemi <beniphemeh11@yahoo.com>
2025-06-19 14:35:14 +0100

    eslint error

--- src/core/database/migrations/20250219075000-create-users.js
+++ src/core/database/migrations/20250219075000-create-users.js
@@ -1,6 +1,7 @@
 'use strict';
-const baseModelMigration = require('../base-model/base-model.migration');
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+const baseModelMigration = require('../base-model/base-model.migration');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
