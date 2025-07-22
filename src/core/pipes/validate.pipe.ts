92827d9a14ebc5f55a75a02cbf76d8bc8237a0f1
Adewole oluwanifemi <beniphemeh11@yahoo.com>
2025-07-22 08:53:19 +0100

    es lint errors for test and users controller

--- src/core/pipes/validate.pipe.ts
+++ src/core/pipes/validate.pipe.ts
@@ -31,28 +31,3 @@ export class ValidateInputPipe implements PipeTransform {
     return !types.includes(metatype);
   }
 }
-
-// import {
-//   Injectable,
-//   ArgumentMetadata,
-//   BadRequestException,
-//   ValidationPipe,
-//   UnprocessableEntityException,
-// } from '@nestjs/common';
-
-// @Injectable()
-// export class ValidateInputPipe extends ValidationPipe {
-//   public async transform(value, metadata: ArgumentMetadata) {
-//     try {
-//       return await super.transform(value, metadata);
-//     } catch (e) {
-//       if (e instanceof BadRequestException) {
-//         throw new UnprocessableEntityException(this.handleError(e));
-//       }
-//     }
-//   }
-
-//   private handleError(errors) {
-//     return errors.map((error) => error.constraints);
-//   }
-// }
